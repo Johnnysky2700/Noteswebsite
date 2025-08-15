70c66ab1538be9939c9ff20e71deb0405ce34d8d
iamjohnnysky <vanpasie20@gmail.com>
2025-08-15 12:02:24 -0700

    login and sign-up pages set up, flash messages set up and request imported for methods to GET and POST

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+     if request.method == 'POST':
+         email = request.form.get('email')
+         firstname = request.form.get('firstname')
+         password1 = request.form.get('password1') 
+         password2 = request.form.get('password2')
+
+         if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+         elif len(firstname) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+         elif password1 != password2:
+            flash('passwords don\'t match.', category='error')
+         elif len(password1) < 7:
+            flash('password must be at least 7 characters.', category='error')
+         else:
+             # add user to database
+            flash('Account created.', category='success')
+            
+     return render_template("sign_up.html")
 
