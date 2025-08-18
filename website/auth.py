686cf3992d49ca10f2d20a46961fd4062f90ae81
iamjohnnysky <vanpasie20@gmail.com>
2025-08-18 05:46:36 -0700

    login and sign_up is working successfully

--- website/auth.py
+++ website/auth.py
@@ -7,7 +7,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -22,10 +34,10 @@ def sign_up():
          password1 = request.form.get('password1') 
          password2 = request.form.get('password2')
 
-         # existing_user = User.query.filter_by(email=email).first()
-         # if existing_user:
-         #    flash('Email already exists.', category='error')
-         if len(email) < 4:
+         existing_user = User.query.filter_by(email=email).first()
+         if existing_user:
+             flash('Email already exists.', category='error')
+         elif len(email) < 4:
             flash('Email must be greater than 4 characters.', category='error')
          elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
